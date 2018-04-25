4abfd304bd4fc73f5799f53a96104ebbc9dccc44
Alex Terner <AlexTerner@gmail.com>
2018-04-25 10:34:30 +0300

    First commit

---
+++ src/main/java/com/mycompany/springboot/kafka/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.springboot.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
